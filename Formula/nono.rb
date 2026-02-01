88ada86e77aa1ec930017199351476d89eb0e8f0
Luke Hinds <lukehinds@gmail.com>
2026-02-01 16:53:14 +0000

    Update SHA256 checksums for v0.1.0

--- Formula/nono.rb
+++ Formula/nono.rb
@@ -7,11 +7,11 @@ class Nono < Formula
   on_macos do
     on_arm do
       url "https://github.com/lukehinds/nono/releases/download/v0.1.0/nono-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "54c7cc43fac828fbe7d0f38a03373cd1453355fbce3d638fbc053a4f49aabf56"
     end
     on_intel do
       url "https://github.com/lukehinds/nono/releases/download/v0.1.0/nono-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_X64_SHA256"
+      sha256 "c146facc93b927a5cf3097d62539177e254183ad06ec03bf1dc248791060d042"
     end
   end
 
